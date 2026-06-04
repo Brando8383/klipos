8bb28a04c1ddc2b5486629a2e3a0aff499c33e2a
Brando8383 <white90stang@yahoo.com>
2026-06-04 02:03:24 +0000

    Add genimage config and post-image script for flashable KlipOS disk image

---
+++ board/klipos/post-image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# KlipOS post-image script
+# Assembles bootable disk image from Buildroot output
+
+BINARIES_DIR=$1
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+echo "Assembling KlipOS disk image..."
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+    --rootpath "${TARGET_DIR}" \
+    --tmppath "${GENIMAGE_TMP}" \
+    --inputpath "${BINARIES_DIR}" \
+    --outputpath "${BINARIES_DIR}" \
+    --config "${GENIMAGE_CFG}"
+
+echo "KlipOS disk image complete."
